aa6bc393af4b1c6d8a3d39c9feea0e5ac508a9b1
asma ameen alshanqiti <asmaa.alshanqiti@gmail.com>
2020-07-25 10:35:55 +0300

    upload

---
+++ build/precache-manifest.36820ee3c313d2050f8e9aa2f693bc73.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a762a267c30e3a68d73b",
+    "url": "/books/static/css/main.df17159b.chunk.css"
+  },
+  {
+    "revision": "a762a267c30e3a68d73b",
+    "url": "/books/static/js/main.a762a267.chunk.js"
+  },
+  {
+    "revision": "35c948a2017f32b100b9",
+    "url": "/books/static/js/1.35c948a2.chunk.js"
+  },
+  {
+    "revision": "7508a2c18729acf59835",
+    "url": "/books/static/js/runtime~main.7508a2c1.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/books/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/books/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/books/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "d3d745ac429acd049547128f4f135459",
+    "url": "/books/index.html"
+  }
+];
